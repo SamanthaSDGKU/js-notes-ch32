746863dbd6a310d134f6d00cf47a45a4f8ce60d4
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2022-09-27 18:52:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("This is the console");
+
+let studentName = "Frank"; // string "" '' ``
+let studentAge = 20; // number 1,10000,1.3,0.5
+let isActive = true; // boolean true or false
+
+
+document.write("<p>Name: " + studentName + "</p>"); // concatenation
+document.write("<p> Age: " + studentAge + "</p>"); 
+document.write("<p> Active: " + isActive + "</p>"); 
+
